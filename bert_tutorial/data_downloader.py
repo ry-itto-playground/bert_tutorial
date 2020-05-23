7a9b033cb91c6f6e74866be0dfa6c0901c100634
ryoya ito <horusu.dqx@gmail.com>
2020-05-24 05:03:21 +0900

    extract file

--- bert_tutorial/data_downloader.py
+++ bert_tutorial/data_downloader.py
@@ -1,9 +1,13 @@
 import urllib.request
 import os
 import sys
+import shutil
+import tarfile
 
 DATA_DIR = f'{os.path.dirname(__file__)}/data'
 DATASET_FILE = 'yelp_review_full_csv.tgz'
+TRAIN_CSV_FILE = 'train.csv'
+TEST_CSV_FILE = 'test.csv'
 URL = 'https://s3.amazonaws.com/fast-ai-nlp/yelp_review_full_csv.tgz'
 
 
@@ -18,17 +22,34 @@ def progress(block_count, block_size, total_size):
 def download():
     if not os.path.exists(DATA_DIR):
         os.mkdir(DATA_DIR)
-    zip_file: str = '/'.join([DATA_DIR, DATASET_FILE])
+    tgz_file: str = '/'.join([DATA_DIR, DATASET_FILE])
 
-    if not os.path.exists(zip_file):
-        print(f'Download to {zip_file}')
+    if not os.path.exists(tgz_file):
+        print(f'Download to {tgz_file}')
         urllib.request.urlretrieve(
             url=URL,
-            filename=zip_file,
+            filename=tgz_file,
             reporthook=progress)
         print('Done.')
     else:
         print('Skip download dataset.')
+
+    extract_tgz_file(tgz_file)
+
+
+def extract_tgz_file(filepath: str):
+    print('extract download tgz file')
+    with tarfile.open(filepath) as tar:
+        tar.extractall(
+            path=DATA_DIR,
+        )
+
+    extracted_dir = f'{DATA_DIR}/yelp_review_full_csv'
+    for file in [TRAIN_CSV_FILE, TEST_CSV_FILE]:
+        shutil.copy(f'{extracted_dir}/{file}', DATA_DIR)
+    shutil.rmtree(extracted_dir)
+
+    print('Done.')
 
 
 if __name__ == "__main__":
